b0e97fd064fe541b633907cd5bd1080e7a033dda
Maevskaya Anastasia <maevskaya.ana@gmail.com>
2023-07-16 21:15:59 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI, Request, UploadFile, File
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, UploadFile, File, status
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
-from funcs import crop_img, create_html_content
+from funcs import crop_img, acc_img, create_html_content
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 IMAGE_DIR = "images"
 cropped_path = os.path.join(IMAGE_DIR, "cropped.png")
+acc_filename = "cropped_out.png"
+acc_path = os.path.join(IMAGE_DIR, acc_filename)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -23,7 +34,22 @@ def main(request: Request):
 
 
 @app.post("/result", response_class=HTMLResponse)
-def process_img(request: Request, file: UploadFile = File(...)):
+def process_raw_img(request: Request, file: UploadFile = File(...)):
     crop_img(file)
-    return templates.TemplateResponse("result.html", {"request": request,
-                                                      "content": create_html_content(file.filename)})
+    return templates.TemplateResponse("cropped_result.html", {"request": request,
+                                                              "content": create_html_content(cropped_path,
+                                                                                             file.filename)})
+
+
+@app.post("/enhanced_result", response_class=HTMLResponse)
+def process_cropped_img(request: Request):
+    acc_img()
+    redirect_url = request.url_for("enhanced_result", **{'filename': acc_filename})
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/enhanced_result/{filename}", response_class=HTMLResponse)
+def enhanced_result(request: Request, filename: str):
+    return templates.TemplateResponse("enhanced_result.html", {"request": request,
+                                                               "content": create_html_content(acc_path,
+                                                                                              filename)})
